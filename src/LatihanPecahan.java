2e1b218d9ec0ee93f8c8bb05f23a23a071beb6d7
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-10-16 09:24:20 +0700

    Add files via upload

---
+++ src/LatihanPecahan.java
@@ -0,0 +1,21 @@
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Latihan menggunakan Pecahan
+ *
+ */
+
+public class LatihanPecahan {
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+        
+    }
+}
